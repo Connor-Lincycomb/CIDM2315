7999c0661fdeca300b6c635a3587be9fdd2b2895
Connor-Lincycomb <156738155+Connor-Lincycomb@users.noreply.github.com>
2024-01-15 21:43:52 -0600

    Add files via upload

---
+++ Homework1.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("I'm connor!");
+    }
+}
+
